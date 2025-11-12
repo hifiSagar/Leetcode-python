e8f14da8a85006efb3385dd8567d52718fad91c3
SAGAR T <ssagu6661@gmail.com>
2025-11-12 22:24:32 +0530

    Add files via upload

---
+++ python21.py
@@ -0,0 +1,24 @@
+course="Python Programming"
+print(course.upper())
+print(course.lower())
+print(course.title())
+
+print("\n")
+
+print("!! NOW IAM PRNTING A SOME CALCULACTING OPERATIONS !!")
+print("\n")
+print("Enter the value for >>>\n1.Addition\n2.Subtraction\n3.Multiplication\n4.Division\n")
+A=int(input("Enter the 1st value: "))
+
+B=int(input("Enter the 2nd value: "))
+c=A,B
+switch(c)
+
+case: 1
+print(A+B)
+case: 2
+print(A-B)
+case: 3
+print(A*B)
+case: 4
+print(A/B)
