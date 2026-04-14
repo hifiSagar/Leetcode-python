1dad06fbf2c0749872a5a950db7525c03de5f5a4
SAGAR T <ssagu6661@gmail.com>
2026-04-14 17:06:22 +0530

    Add files via upload

---
+++ python52.py
@@ -0,0 +1,9 @@
+class Solution:
+    def intersection(self, nums1, nums2):
+        result = []
+        
+        for i in nums1:
+            if i in nums2 and i not in result:
+                result.append(i)
+        
+        return result
