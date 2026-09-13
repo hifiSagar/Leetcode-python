17861feeea929ff6a31b1c30fc3896f2750a86b0
SAGAR T <ssagu6661@gmail.com>
2026-09-13 18:30:20 +0530

    Add files via upload

--- python59.py
+++ python59.py
@@ -1,16 +1,27 @@
 class Solution:
-    def detectCycle(self, head):
-        slow = head
-        fast = head
+    def reorderList(self, head):
+        slow = fast = head
 
         while fast and fast.next:
             slow = slow.next
             fast = fast.next.next
-            if slow == fast:
-                slow = head
-                while slow != fast:
-                    slow = slow.next
-                    fast = fast.next
-                return slow
 
-        return None
+        prev = None
+        while slow:
+            nxt = slow.next
+            slow.next = prev
+            prev = slow
+            slow = nxt
+
+        first = head
+        second = prev
+
+        while second.next:
+            temp1 = first.next
+            temp2 = second.next
+
+            first.next = second
+            second.next = temp1
+
+            first = temp1
+            second = temp2
